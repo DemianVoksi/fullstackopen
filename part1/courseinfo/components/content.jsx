b739075d6386af9308e036c6543fa0fb2f493ea6
Demian Voksi <demian.voksi@gmail.com>
2025-03-12 20:45:33 +0100

    1.2

--- part1/courseinfo/components/content.jsx
+++ part1/courseinfo/components/content.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
+import Part1 from './part1';
+import Part2 from './part2';
+import Part3 from './part3';
 
 const Content = ({ ...props }) => {
 	return (
 		<div>
-			<p>
-				{props.part1} {props.exercises1}
-			</p>
-			<p>
-				{props.part2} {props.exercises2}
-			</p>
-			<p>
-				{props.part3} {props.exercises3}
-			</p>
+			<Part1 part1={props.part1} exercises1={props.exercises1} />
+			<Part2 part2={props.part2} exercises2={props.exercises2} />
+			<Part3 part3={props.part3} exercises3={props.exercises3} />
 		</div>
 	);
 };
