504a697a09a63b0c03a724b9f12a43796238a08e
Demian Voksi <demian.voksi@gmail.com>
2025-03-13 23:16:25 +0100

    1.7

--- part1/unicafe/src/components/statistics.jsx
+++ part1/unicafe/src/components/statistics.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 
 const Statistics = ({ good, neutral, bad }) => {
+	let totalFeedback = good + neutral + bad;
+
+	function getAverageScore(good, neutral, bad) {
+		if (good + neutral + bad === 0) {
+			return 'none';
+		} else {
+			return (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad);
+		}
+	}
+
+	function getPercentage(positive, total) {
+		const percentage = (positive / total) * 100;
+		if (percentage > 0) {
+			return `${percentage}%`;
+		} else {
+			return '0%';
+		}
+	}
+
+	let averageScore = getAverageScore(good, neutral, bad);
+	let percentage = getPercentage(good, totalFeedback);
+
 	return (
 		<div>
 			<h1>statistics</h1>
 			<p>good: {good}</p>
 			<p>neutral: {neutral}</p>
 			<p>bad: {bad}</p>
+			<p>all: {totalFeedback}</p>
+			<p>average: {averageScore}</p>
+			<p>positive: {percentage}</p>
 		</div>
 	);
 };
