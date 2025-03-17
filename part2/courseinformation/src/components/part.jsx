33435feb103e84304a19a263acfe832f80ae2128
Demian Voksi <demian.voksi@gmail.com>
2025-03-17 20:37:45 +0100

    2.1 fixed the forgotten exercises in the html

--- part2/courseinformation/src/components/part.jsx
+++ part2/courseinformation/src/components/part.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Part = ({ part }) => {
-	return <div>{part.name}</div>;
+	return (
+		<div>
+			<p>
+				{part.name} {part.exercises}
+			</p>
+		</div>
+	);
 };
 
 export default Part;
