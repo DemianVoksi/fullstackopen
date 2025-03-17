57cad32fba3ccb75e3a638447abf22e0acb08b24
Demian Voksi <demian.voksi@gmail.com>
2025-03-17 21:21:06 +0100

    2.4, 2.5: Course is already a separate module

--- part2/courseinformation/src/App.jsx
+++ part2/courseinformation/src/App.jsx
@@ -4,35 +4,55 @@ import Header from './components/header';
 import Total from './components/total';
 
 function App() {
-	const course = {
-		name: 'Half Stack application development',
-		parts: [
-			{
-				name: 'Fundamentals of React',
-				exercises: 10,
-				id: 1,
-			},
-			{
-				name: 'Using props to pass data',
-				exercises: 7,
-				id: 2,
-			},
-			{
-				name: 'State of a component',
-				exercises: 14,
-				id: 3,
-			},
-			{
-				name: 'Redux',
-				exercises: 11,
-				id: 4,
-			},
-		],
-	};
+	const courses = [
+		{
+			name: 'Half Stack application development',
+			id: 1,
+			parts: [
+				{
+					name: 'Fundamentals of React',
+					exercises: 10,
+					id: 1,
+				},
+				{
+					name: 'Using props to pass data',
+					exercises: 7,
+					id: 2,
+				},
+				{
+					name: 'State of a component',
+					exercises: 14,
+					id: 3,
+				},
+				{
+					name: 'Redux',
+					exercises: 11,
+					id: 4,
+				},
+			],
+		},
+		{
+			name: 'Node.js',
+			id: 2,
+			parts: [
+				{
+					name: 'Routing',
+					exercises: 3,
+					id: 1,
+				},
+				{
+					name: 'Middlewares',
+					exercises: 7,
+					id: 2,
+				},
+			],
+		},
+	];
 	return (
 		<div>
-			<Course course={course} />
-			<Total parts={course.parts} />
+			{courses.map((course) => (
+				<Course course={course} key={course.id} />
+			))}
 		</div>
 	);
 }
--- part2/courseinformation/src/components/course.jsx
+++ part2/courseinformation/src/components/course.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Content from './content';
 import Header from './header';
+import Total from './total';
 
 const Course = ({ course }) => {
 	return (
 		<div>
 			<Header name={course.name} />
 			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</div>
 	);
 };
