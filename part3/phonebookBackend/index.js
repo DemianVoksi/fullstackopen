4c145c1c2ccc0bf4979a5f46f9cf24bac47ddc10
Demian Voksi <demian.voksi@gmail.com>
2025-03-21 14:14:50 +0100

    3.2, 3.3, 3.4, 3.5 & 3.6

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -22,7 +22,19 @@ let persons = [
 		name: 'Mary Poppendieck',
 		number: '39-23-6423122',
 	},
+	{
+		id: '5',
+		name: 'Steven Stephen',
+		number: '45-67-1122334',
+	},
 ];
+
+const currentDateTime = new Date().toString();
+
+function getRandomId() {
+	const id = Math.floor(Math.random() * 1000);
+	return id;
+}
 
 app.use(express.json());
 
@@ -30,8 +42,57 @@ app.get('/', (request, response) => {
 	response.send('<h1>Welcome to phonebook backend.</h1>');
 });
 
+app.get('/info', (request, response) => {
+	response.send(`
+		<p>Phonebook has info for ${persons.length} people.</p>
+		<p>${currentDateTime}</p>
+		`);
+});
+
 app.get('/api/persons', (request, response) => {
 	response.send(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = request.params.id;
+	const person = persons.find((person) => person.id === id);
+	if (person) {
+		response.send(person).status(200).end();
+	} else {
+		response.status(400).json({
+			error: 'content missing',
+		});
+	}
+});
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+	const names = persons.map((person) => person.name);
+
+	if (!body.name || !body.number) {
+		response.status(400).json({
+			error: 'Provided information is incomplete',
+		});
+	} else if (names.includes(body.name)) {
+		response.status(400).json({
+			error: `${body.name} is already in the Phonebook. Name must be unique.`,
+		});
+	} else {
+		const person = {
+			id: getRandomId(),
+			name: body.name,
+			number: body.number,
+		};
+
+		persons = persons.concat(person);
+		response.json(person);
+	}
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = request.params.id;
+	persons = persons.filter((person) => person.id !== id);
+	response.status(204).end();
 });
 
 const PORT = 3001;
