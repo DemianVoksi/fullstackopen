cd9218e014aa1bae01ed9ea29cb262a249d55d05
Demian Voksi <demian.voksi@gmail.com>
2025-04-02 19:07:53 +0200

    4.10

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,15 +1,20 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const { errorHandler } = require('../utils/middleware');
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({});
 	response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => {
-	const blog = new Blog(request.body);
-	const result = await blog.save();
-	response.status(201).json(result);
+blogsRouter.post('/', async (request, response, next) => {
+	try {
+		const blog = new Blog(request.body);
+		const result = await blog.save();
+		response.status(201).json(result);
+	} catch (error) {
+		errorHandler(error, request, response, next);
+	}
 });
 
 module.exports = blogsRouter;
